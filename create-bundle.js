2134db3bf13f98e2db13196a8384335158041704
Dylanflini <42789063+Dylanflini@users.noreply.github.com>
2020-11-25 11:56:50 -0300

    create bundle file script

---
+++ create-bundle.js
@@ -0,0 +1,13 @@
+require( 'esbuild' ).build( {
+  entryPoints: [ './static/js/index.js' ],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  splitting: true,
+  format: 'esm',
+  outdir: 'build/js',
+  // define: { 'process.env.NODE_ENV': '"production"' },
+  target: [ 'chrome58', 'firefox57', 'safari11', 'edge16' ],
+} ).catch( () => {
+  process.exit( 1 )
+} )
